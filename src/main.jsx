8c4262620b29369ceb715595bda44f4657c6529d
ToEzBit <jirayut52328172@hotmail.com>
2023-03-06 19:26:37 +0700

    fix : action

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,6 @@ import { ChakraProvider } from "@chakra-ui/react";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const queryClient = new QueryClient();
 
@@ -15,7 +14,6 @@ ReactDOM.createRoot(document.getElementById("root")).render(
       <BrowserRouter>
         <QueryClientProvider client={queryClient}>
           <App />
-          <ReactQueryDevtools initialIsOpen={false} />
         </QueryClientProvider>
       </BrowserRouter>
     </ChakraProvider>
